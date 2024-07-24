5464ef0e600ade3c76a4074d1611cb36b54a5a9c
animanoukiann <ani.manukayn@student.rau.am>
2024-07-24 16:30:05 +0400

    fixing parts which can cause a seg fault

--- README.md
+++ README.md
@@ -21,3 +21,37 @@ to run the project, use the following command
 ```
 ./output/main
 ```
+I added argument parsers which are
+
+--addInstructor
+
+for this parser you can give one, two or three argumets respectively name of the instructor, name of the course, preffered time which should be like this "Monday 12:50 14:25"
+
+Ex. 
+```
+./output/main --addInstructor "John B" "Intro to ML" "Monday 12:50 14:25"
+./output/main --addInstructor "John B"
+./output/main --addInstructor "John B" "Intro to ML"
+```
+--addCourse
+
+for this parser you can give one or two argumemts respectively name of the course,  preffered time which should be like this "Monday 12:50 14:25"
+
+Ex. 
+```
+./output/main --addCourse "Intro to ML" "Monday 12:50 14:25"
+./output/main --addCourse "Intro to ML"
+```
+
+--addTimeslot
+
+for this parser you need to give three arguments respectively day, start time and end time
+
+Ex. 
+```
+./output/main --addTimeslot "Monday" "12:50" "14:25"
+```
+
+--scedule
+
+which is for calling scheduling algorithm for inputed data
--- test/main.cpp
+++ test/main.cpp
@@ -19,12 +19,10 @@ int main(int argc, const char **argv)
     ArgumentParser parser("UniversityTimetable", "University Timetable System");
 
     parser.add_argument("--addInstructor", "addInstructor")
-        .count(3)
         .description("Adds a new instructor by their name, preferred course and timeslot")
         .required(false);
 
     parser.add_argument("--addCourse", "addCourse")
-        .count(2)
         .description("Adds a new course by its name and preferred time")
         .required(false);
 
@@ -56,34 +54,54 @@ int main(int argc, const char **argv)
 
     if (parser.exists("addInstructor"))
     {
-        //here are some parts that can cause a seg fault if you give input improper way
-        //so it must have 3 arguments and be careful with the 2nd argumet 
-        //example` --addInstructor "John B" "Thursday 14:35 16:10" "MathPhys Eq"
         std::vector<std::string> inst_info = parser.get<std::vector<std::string>>("addInstructor");
-        std::vector<std::string> time = splitStringBySpace(inst_info[1]);
-        TimeSlot ts(time[0], time[1], time[2]);
-        Course course(inst_info[2]);
-        Instructor instructor(inst_info[0]);
-        instructor.setAvailability(ts);
-        instructor.setPreferredCourses(course);
-        uni.addInstructor(instructor);
+        if (inst_info.size() <= 3 && inst_info.size() >= 1)
+        {
+            Instructor instructor(inst_info[0]);
+            if (inst_info.size() >= 2)
+            {
+                Course course(inst_info[1]);
+                instructor.setPreferredCourses(course);
+            }
+            if (inst_info.size() == 3)
+            {
+                std::vector<std::string> time = splitStringBySpace(inst_info[2]);
+                TimeSlot ts(time[0], time[1], time[2]);
+                instructor.setAvailability(ts);
+            }
+            uni.addInstructor(instructor);
+        }
+        else
+        {
+            std::cerr << "The max count of arguments for --addInstructor is 3!\n";
+            return (1);
+        }
     }
 
     if (parser.exists("addCourse"))
     {
-        //the same here
-        //example --addCourse "Relational Databases" "Saturday 9:00 10:45"
         std::vector<std::string> course_info = parser.get<std::vector<std::string>>("addCourse");
-        std::vector<std::string> time_1 = splitStringBySpace(course_info[1]);
-        TimeSlot ts1(time_1[0], time_1[1], time_1[2]);
-        Course course(course_info[0], {ts1});
-        uni.addCourse(course);
+        if (course_info.size() == 1)
+        {
+            Course course1(course_info[0]);
+            uni.addCourse(course1);
+        }
+        else if(course_info.size() == 2)
+        {
+            std::vector<std::string> time_1 = splitStringBySpace(course_info[1]);
+            TimeSlot ts1(time_1[0], time_1[1], time_1[2]);
+            Course course(course_info[0], {ts1});
+            uni.addCourse(course);
+        }
+        else
+        {
+            std::cerr << "The max count of arguments for --addCourse is 2!\n";
+            return (1);
+        }
     }
 
     if (parser.exists("addTimeslot"))
     {
-        //the same here
-        //example --addTimeslot "Monday" "12:50" "14:25"
         std::vector<std::string> time_info = parser.get<std::vector<std::string>>("addTimeslot");
         TimeSlot ts2(time_info[0], time_info[1], time_info[2]);
         uni.addTimeSlot(ts2);
@@ -93,16 +111,6 @@ int main(int argc, const char **argv)
 
     University loadedUni;
     loadedUni = loadedUni.loadState("./result.json");
-
-    std::cout << "Loaded Courses:" << std::endl;
-    for (const auto &course : loadedUni.courses) {
-        course.displayInfo();
-    }
-
-    std::cout << "Loaded Instructors:" << std::endl;
-    for (const auto &instructor : loadedUni.instructors) {
-        instructor.displayInfo();
-    }
 
     if (parser.exists("--schedule"))
     {
